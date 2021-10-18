5f57b25c54d883e87d6ef4b1cb4d0657ccf76e4e
Gleb <hopinplay@gmail.com>
2021-10-18 22:21:05 +0300

    fixed code

--- inheritance2/src/by/gsu/epamlab/AbstractPurchase.java
+++ inheritance2/src/by/gsu/epamlab/AbstractPurchase.java
@@ -18,10 +18,6 @@ public abstract class AbstractPurchase implements Comparable<AbstractPurchase> {
         return product;
     }
 
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
     public int getNumber() {
         return number;
     }
