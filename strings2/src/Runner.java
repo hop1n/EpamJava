654f8b92444314d4ff428556594ef22b422eb7e4
Gleb <hopinplay@gmail.com>
2021-11-25 17:45:01 +0300

    code cleanup

--- strings2/src/Runner.java
+++ strings2/src/Runner.java
@@ -4,19 +4,21 @@ import java.util.regex.Pattern;
 
 public class Runner {
     public static void main(String[] args) {
+        final String ERR_MESSAGE = "there is no data";
         try {
             final String INPUT_PROPERTIES = "in";
+            final String ERR_LINES_OUT = "error-lines = ";
+            final String SUM_OUT = "sum = ";
             ResourceBundle rb = ResourceBundle.getBundle(INPUT_PROPERTIES);
             Enumeration<String> keys = rb.getKeys();
             int errorLines = 0;
             double sum = 0.0;
             while (keys.hasMoreElements()) {
-                String key;
                 try {
-                    String regex1 = "index.*";
-                    String regex2 = "index([1-9]\\d*)";
-                    String regex3 ="([1-9])(\\d*)";
-                    key = keys.nextElement();
+                    final String regex1 = "index.*";
+                    final String regex2 = "index([1-9]\\d*)";
+                    final String regex3 ="([1-9])(\\d*)";
+                    String key = keys.nextElement();
                     String regex = regex1;
                     Pattern pattern = Pattern.compile(regex);
                     Matcher matcher = pattern.matcher(key);
@@ -45,10 +47,10 @@ public class Runner {
                     errorLines++;
                 }
             }
-            System.out.println("sum = " + sum);
-            System.out.println("error-lines = " + errorLines);
+            System.out.println(SUM_OUT + sum);
+            System.out.println(ERR_LINES_OUT + errorLines);
         } catch (MissingResourceException e) {
-            System.out.println(e.getMessage());
+            System.out.println(ERR_MESSAGE);
         }
     }
 }
