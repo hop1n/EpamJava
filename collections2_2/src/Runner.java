c55e3b1036d993315e5e7d9989213ad1de3380e4
Gleb <hopinplay@gmail.com>
2022-03-02 02:12:08 +0300

    first implementationt of collections2_2

---
+++ collections2_2/src/Runner.java
@@ -0,0 +1,40 @@
+import by.epam.lab.Constants;
+import by.epam.lab.LenNum;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.*;
+
+public class Runner {
+    public static void main(String[] args) {
+        Set<LenNum> setNumLen = new HashSet<LenNum>();
+        String regex = Constants.REGEX;
+        try (Scanner sc = new Scanner(new FileReader(Constants.PATH))) {
+            sc.useLocale(Locale.ENGLISH);
+            while (sc.hasNextLine()) {
+                String csvLine = sc.nextLine();
+                String[] parts = csvLine.split(regex);
+                double x1 = Double.parseDouble(parts[Constants.X1_INDEX]);
+                double y1 = Double.parseDouble(parts[Constants.Y1_INDEX]);
+                double x2 = Double.parseDouble(parts[Constants.X2_INDEX]);
+                double y2 = Double.parseDouble(parts[Constants.Y2_INDEX]);
+                int l = (int) Math.round(Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)));
+                LenNum lenNum = new LenNum(l);
+                lenNum = new LenNum(l);
+                setNumLen.add(lenNum);
+            }
+            List<LenNum> list = new ArrayList<>(setNumLen);
+            Collections.sort(list, new Comparator<LenNum>() {
+                @Override
+                public int compare(LenNum a, LenNum b) {
+                    return a.getNum() - b.getNum();
+                }
+            });
+            for (LenNum lenNum : setNumLen) {
+                System.out.println(lenNum);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
