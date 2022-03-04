95d26798ea7ec6811439090f0b6f09479c8f6461
Gleb Savelyeu <hopinplay@gmail.com>
2022-03-05 00:39:58 +0300

    editing comparator signature

--- collections2_3/src/by/epam/lab/NumComparator.java
+++ collections2_3/src/by/epam/lab/NumComparator.java
@@ -3,7 +3,7 @@ package by.epam.lab;
 import java.util.Comparator;
 import java.util.Map;
 
-public class NumComparator<E> implements Comparator<Map.Entry<Integer, Integer>> {
+public class NumComparator implements Comparator<Map.Entry<Integer, Integer>> {
     public int compare(Map.Entry<Integer, Integer> e1, Map.Entry<Integer, Integer> e2) {
         return e2.getValue() - e1.getValue();
     }
