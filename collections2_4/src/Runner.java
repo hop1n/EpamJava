c0475e713999bc36c42a837768f1fa0550c10c36
Gleb Savelyeu <hopinplay@gmail.com>
2022-03-06 11:43:15 +0300

    first implementation collections2_4

---
+++ collections2_4/src/Runner.java
@@ -0,0 +1,40 @@
+import by.epam.lab.NumComparator;
+import by.epam.lab.Constants;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.*;
+
+public class Runner {
+    public static void main(String[] args) {
+        Map<Integer, Integer> mapNumLen = new HashMap<>();
+        try (Scanner sc = new Scanner(new FileReader(Constants.PATH))) {
+            sc.useLocale(Locale.ENGLISH);
+            while (sc.hasNextLine()) {
+                String csvLine = sc.nextLine();
+                String[] parts = csvLine.split(Constants.REGEX);
+                double x1 = Double.parseDouble(parts[Constants.X1_INDEX]);
+                double y1 = Double.parseDouble(parts[Constants.Y1_INDEX]);
+                double x2 = Double.parseDouble(parts[Constants.X2_INDEX]);
+                double y2 = Double.parseDouble(parts[Constants.Y2_INDEX]);
+                int l = (int) Math.round(Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)));
+                if (mapNumLen.containsKey(l)) {
+                    mapNumLen.put(l, mapNumLen.get(l) + 1);
+                } else {
+                    mapNumLen.put(l, 1);
+                }
+            }
+            List<Map.Entry<Integer, Integer>> list = new ArrayList<>(mapNumLen.entrySet());
+            Collections.sort(list, new NumComparator());
+            printCollections(list);
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static<I> void printCollections(Collection<I> coll){
+        for (I collection : coll){
+            System.out.println(collection);
+        }
+    }
+}
---
+++ collections2_4/src/by/epam/lab/Constants.java
@@ -0,0 +1,11 @@
+package by.epam.lab;
+
+public class Constants {
+    public final static String DELIMITER = ";";
+    public final static String PATH = "src/in.csv";
+    public final static String REGEX = "[\\s();]+";
+    public final static int X1_INDEX = 1;
+    public final static int X2_INDEX = 3;
+    public final static int Y1_INDEX = 2;
+    public final static int Y2_INDEX = 4;
+}
