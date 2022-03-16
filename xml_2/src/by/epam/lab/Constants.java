4f890ad18eef35821418cda668c60985b6bee3e2
Gleb Savelyeu <hopinplay@gmail.com>
2022-03-16 10:41:05 +0300

    first implementation of xml_2 task

---
+++ xml_2/src/by/epam/lab/Constants.java
@@ -0,0 +1,12 @@
+package by.epam.lab;
+
+public class Constants {
+    public final static int NAME_INDEX = 0;
+    public final static int DATE_INDEX = 1;
+    public final static int MARK_INDEX = 2;
+    public final static String DELIMITER = ";";
+    public final static String DATE_FORMAT = "dd.MM.yyyy";
+    public final static String PATH = "src/results.xml";
+    public final static int CONVERT = 10;
+    public final static String  DOT = ".";
+}
