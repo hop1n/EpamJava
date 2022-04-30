7403e78b43abe4ead232d083896b57edb2301965
Gleb Savelyeu <hopinplay@gmail.com>
2022-04-30 21:57:24 +0300

    refactored runner

--- java8/src/Runner.java
+++ java8/src/Runner.java
@@ -23,19 +23,30 @@ public class Runner {
         trainee.add(new ExtraTrial("rachele", 80, 90, 50));
         trainee.add(new ExtraTrial("rachele", 50, 90, 90));
         trainee.forEach(System.out::println);
-        System.out.println(TEST_PASSED + trainee.stream().filter(Trial::isPassed).count());
+        System.out.println(TEST_PASSED + trainee.stream()
+                .filter(Trial::isPassed)
+                .count());
         System.out.println(SORTED);
         //trainee.stream().sorted(Trial::compareTo).forEach(System.out::println); why is this sort not working ?
         trainee.sort(Comparator.comparingInt(Trial::getSum));
-        trainee.stream().map(Trial::getSum).forEach(System.out::println);
+        trainee.stream()
+                .map(Trial::getSum)
+                .forEach(System.out::println);
         List<Trial> failedTrials = new ArrayList<>();
         List<Trial> clonedTrials = new ArrayList<>();
-        trainee.stream().map(Trial::getClone).forEach(clonedTrials::add);
-        clonedTrials.stream().filter(t -> !t.isPassed()).forEach(failedTrials::add);
+        trainee.stream()
+                .map(Trial::getClone)
+                .forEach(clonedTrials::add);
+        clonedTrials.stream()
+                .filter(t -> !t.isPassed())
+                .forEach(failedTrials::add);
         failedTrials.forEach(System.out::println);
         failedTrials.forEach(Trial::clearMarks);
-        System.out.println(TEST_FAILED + failedTrials.stream().noneMatch(Trial::isPassed));
-        int[] sums = trainee.stream().mapToInt(Trial::getSum).toArray();
+        System.out.println(TEST_FAILED + failedTrials.stream()
+                .noneMatch(Trial::isPassed));
+        int[] sums = trainee.stream()
+                .mapToInt(Trial::getSum)
+                .toArray();
         Arrays.stream(sums).forEach(System.out::println);
     }
 }
