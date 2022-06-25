65ebc220bb2f6797a94c2da23bbd6dc481053410
Gleb <hopinplay@gmail.com>
2022-06-25 23:37:50 +0300

    fixed code #4

--- threads1_3/src/by/epam/lab/TrialsWriter.java
+++ threads1_3/src/by/epam/lab/TrialsWriter.java
@@ -5,29 +5,28 @@ import java.io.IOException;
 import java.util.Queue;
 
 public class TrialsWriter implements Runnable {
-    private final Queue<Trial> QUEUE;
-    private final String FILE_NAME;
+    private final Queue<Trial> buffer;
+    private final String fileName;
 
-    public TrialsWriter(Queue<Trial> queue, String path){
-        this.QUEUE = queue;
-        FILE_NAME = path;
-
+    public TrialsWriter(Queue<Trial> queue, String path) {
+        this.buffer = queue;
+        fileName = path;
     }
+
     @Override
     public void run() {
-        try(FileWriter writer = new FileWriter(FILE_NAME))
-        {
+        try (FileWriter writer = new FileWriter(fileName)) {
             while (true) {
-                if(QUEUE.isEmpty()){
+                if (buffer.isEmpty()) {
                     break;
                 }
-                Trial trial = QUEUE.remove();
+                Trial trial = buffer.remove();
                 if (trial == null) {
                     break;
                 }
-                writer.write(trial+"\n");
+                writer.write(trial + "\n");
             }
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
